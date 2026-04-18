175dd0e85a30f080e5994f45ca555df4c4860f84
Yoricya <xl5002xf@gmail.com>
2026-04-19 01:38:54 +0500

    TweakSettingsUi refractoring

--- TMessagesProj/src/main/java/wgram/activities/TweakSettingsActivity.java
+++ TMessagesProj/src/main/java/wgram/activities/TweakSettingsActivity.java
@@ -10,14 +10,17 @@ import org.telegram.messenger.R;
 import org.telegram.ui.ActionBar.AlertDialog;
 import org.telegram.ui.ActionBar.BaseFragment;
 import org.telegram.ui.ActionBar.Theme;
-import org.telegram.ui.Cells.TextCheckCell;
-import org.telegram.ui.Cells.TextSettingsCell;
+import org.telegram.ui.Cells.*;
 import org.telegram.ui.Components.BulletinFactory;
 import org.telegram.ui.Components.LayoutHelper;
+
+import wgram.DeletedMessages;
 import wgram.TweakSettings;
+import wgram.WGram;
 
 public class TweakSettingsActivity extends BaseFragment {
 
+    LinearLayout linearLayout;
     @Override
     public View createView(Context context) {
         actionBar.setTitle("Tweak Settings");
@@ -25,56 +28,61 @@ public class TweakSettingsActivity extends BaseFragment {
         actionBar.setAllowOverlayTitle(true);
         actionBar.setOnClickListener(v -> finishFragment());
 
-        ScrollView scrollView = new ScrollView(context);
-        scrollView.setFillViewport(true);
-        LinearLayout linearLayout = new LinearLayout(context);
+        linearLayout = new LinearLayout(context);
         linearLayout.setOrientation(LinearLayout.VERTICAL);
         linearLayout.setPadding(0, 0, 0, AndroidUtilities.dp(16));
 
-        scrollView.addView(linearLayout, LayoutHelper.createFrame(
-                LayoutHelper.MATCH_PARENT, LayoutHelper.WRAP_CONTENT));
+        // Setup views
+        setupHeader(context);
+        setupBody(context);
 
-        // Keep Deleted
-        TextCheckCell keepDeletedCell = new TextCheckCell(context);
-        keepDeletedCell.setTextAndCheck("Keep Deleted Messages", wgram.TweakSettings.KeepDeletedMessages, false);
-        keepDeletedCell.setPadding(AndroidUtilities.dp(16), 0, AndroidUtilities.dp(16), 0);
-        keepDeletedCell.setOnClickListener(v -> {
-            boolean newState = !keepDeletedCell.isChecked();
-            keepDeletedCell.setChecked(newState);
-            wgram.TweakSettings.KeepDeletedMessages = newState;
+        // Setup root layouts
+        ScrollView scrollView = new ScrollView(context);
+        scrollView.setFillViewport(true);
+        scrollView.addView(linearLayout, LayoutHelper.createFrame(LayoutHelper.MATCH_PARENT, LayoutHelper.WRAP_CONTENT));
+
+        FrameLayout rootView = new FrameLayout(context);
+        rootView.setBackgroundColor(Theme.getColor(Theme.key_windowBackgroundGray));
+        rootView.addView(scrollView, LayoutHelper.createFrame(LayoutHelper.MATCH_PARENT, LayoutHelper.MATCH_PARENT));
+
+        fragmentView = rootView;
+        return fragmentView;
+    }
+
+    public void setupHeader(Context context) {
+        // WGram info text
+        HeaderCell headerCell = new HeaderCell(context);
+        headerCell.setText("WhitedGram v" + WGram.Ver);
+        linearLayout.addView(headerCell, LayoutHelper.createLinear(LayoutHelper.MATCH_PARENT, LayoutHelper.WRAP_CONTENT));
+
+        // divider
+        linearLayout.addView(new ShadowSectionCell(context), LayoutHelper.createLinear(LayoutHelper.MATCH_PARENT, LayoutHelper.WRAP_CONTENT));
+    }
+
+    public void setupBody(Context context){
+        addBodySwitch(context, "Keep Deleted Messages", TweakSettings.KeepDeletedMessages, true, (v) -> {
+            boolean newState = !((TextCheckCell) v).isChecked();
+            ((TextCheckCell) v).setChecked(newState);
+            TweakSettings.KeepDeletedMessages = newState;
         });
-        linearLayout.addView(keepDeletedCell, LayoutHelper.createLinear(
-                LayoutHelper.MATCH_PARENT, LayoutHelper.WRAP_CONTENT));
 
-        // Typing Hide
-        TextCheckCell typingHideCell = new TextCheckCell(context);
-        typingHideCell.setTextAndCheck("Typing Hide", wgram.TweakSettings.TypingHide, false);
-        typingHideCell.setPadding(AndroidUtilities.dp(16), 0, AndroidUtilities.dp(16), 0);
-        typingHideCell.setOnClickListener(v -> {
-            boolean newState = !typingHideCell.isChecked();
-            typingHideCell.setChecked(newState);
-            wgram.TweakSettings.TypingHide = newState;
+        addBodySwitch(context, "Typing Hide", TweakSettings.TypingHide, true, (v) -> {
+            boolean newState = !((TextCheckCell) v).isChecked();
+            ((TextCheckCell) v).setChecked(newState);
+            TweakSettings.TypingHide = newState;
         });
-        linearLayout.addView(typingHideCell, LayoutHelper.createLinear(
-                LayoutHelper.MATCH_PARENT, LayoutHelper.WRAP_CONTENT));
 
-        // Allow Screenshots
-        TextCheckCell allowScreenshotsCell = new TextCheckCell(context);
-        allowScreenshotsCell.setTextAndCheck("Allow Screenshots", TweakSettings.AllowScreenshots, false);
-        allowScreenshotsCell.setPadding(AndroidUtilities.dp(16), 0, AndroidUtilities.dp(16), 0);
-        allowScreenshotsCell.setOnClickListener(v -> {
-            boolean newState = !allowScreenshotsCell.isChecked();
-            allowScreenshotsCell.setChecked(newState);
+        addBodySwitch(context, "Allow Screenshots", TweakSettings.AllowScreenshots, false, (v) -> {
+            boolean newState = !((TextCheckCell) v).isChecked();
+            ((TextCheckCell) v).setChecked(newState);
             TweakSettings.AllowScreenshots = newState;
         });
-        linearLayout.addView(allowScreenshotsCell, LayoutHelper.createLinear(
-                LayoutHelper.MATCH_PARENT, LayoutHelper.WRAP_CONTENT));
 
-
-        // Clear del-mesg button
+        // Clear deleted messages button
         TextSettingsCell clearDbCell = new TextSettingsCell(context);
         clearDbCell.setText("Clear deleted messages", false);
         clearDbCell.setTextColor(Theme.getColor(Theme.key_text_RedRegular));
+        clearDbCell.setBackgroundColor(Theme.getColor(Theme.key_windowBackgroundWhite));
         clearDbCell.setOnClickListener(v -> {
             AlertDialog.Builder builder = new AlertDialog.Builder(getParentActivity());
             builder.setTitle("Clear deleted messages");
@@ -82,7 +90,7 @@ public class TweakSettingsActivity extends BaseFragment {
 
             // mediaNoRemove
             builder.setNeutralButton("Clear with mediaNoRemove", (d, w) -> {
-                wgram.DeletedMessages.getQueue().postRunnable(() -> {
+                DeletedMessages.getQueue().postRunnable(() -> {
                     Exception e = wgram.DeletedMessages.clearDeletedMessages(true);
                     AndroidUtilities.runOnUIThread(() -> {
                         int flag = R.raw.contact_check;
@@ -100,7 +108,7 @@ public class TweakSettingsActivity extends BaseFragment {
 
             // clear all
             builder.setPositiveButton("Clear", (dialog, w) -> {
-                wgram.DeletedMessages.getQueue().postRunnable(() -> {
+                DeletedMessages.getQueue().postRunnable(() -> {
                     Exception e = wgram.DeletedMessages.clearDeletedMessages(false);
                     AndroidUtilities.runOnUIThread(() -> {
                         int flag = R.raw.contact_check;
@@ -118,22 +126,22 @@ public class TweakSettingsActivity extends BaseFragment {
 
             // cancel
             builder.setNegativeButton("Cancel", (dialog, w) -> {
-                BulletinFactory.of(this).createSimpleBulletin(R.raw.contact_check, "Canceled.").show();
+                BulletinFactory.of(this).createSimpleBulletin(R.raw.hand_1, "Canceled.").show();
             });
 
             // show
             showDialog(builder.create());
         });
-        linearLayout.addView(clearDbCell, LayoutHelper.createLinear(
-                LayoutHelper.MATCH_PARENT, LayoutHelper.WRAP_CONTENT));
 
-        // Root FrameLayout
-        FrameLayout rootView = new FrameLayout(context);
-        rootView.setBackgroundColor(Theme.getColor(Theme.key_windowBackgroundGray));
-        rootView.addView(scrollView, LayoutHelper.createFrame(
-                LayoutHelper.MATCH_PARENT, LayoutHelper.MATCH_PARENT));
+        linearLayout.addView(clearDbCell, LayoutHelper.createLinear(LayoutHelper.MATCH_PARENT, LayoutHelper.WRAP_CONTENT));
+    }
 
-        fragmentView = rootView;
-        return fragmentView;
+    public void addBodySwitch(Context context, String name, boolean isChecked, boolean divider, View.OnClickListener listener) {
+        TextCheckCell cell = new TextCheckCell(context);
+        cell.setTextAndCheck(name, isChecked, divider);
+        cell.setPadding(AndroidUtilities.dp(16), 0, AndroidUtilities.dp(16), 0);
+        cell.setOnClickListener(listener);
+        cell.setBackgroundColor(Theme.getColor(Theme.key_windowBackgroundWhite));
+        linearLayout.addView(cell, LayoutHelper.createLinear(LayoutHelper.MATCH_PARENT, LayoutHelper.WRAP_CONTENT));
     }
 }
